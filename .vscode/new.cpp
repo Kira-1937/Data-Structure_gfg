8ac4d688e596ab5ee57e2a6251f9ff054d20b3ad
kira-1937 <vikramaryasaharan@gmail.com>
2021-06-07 12:24:09 +0530

    commit 2

--- .vscode/new.cpp
+++ .vscode/new.cpp
@@ -3,17 +3,18 @@ using namespace std;
 #include<iostream>
 int main(){
     int arr[]={13,24,46,54,76,12};
-    for {int i=0;i<n;i++}{
-    for{int j=1;j<j+1;j++}{
-    if{(arr[i]+arr[j])==130}{
-    cout<<"true";
-    else{
-    cout<<"false";
-                }
-            }
+    for ( int i=0;i<6;i++){
+    for( int j=1;j<6;j++){
+    if( (arr[i]+arr[j])==130){
+    cout<<"true"<<endl;
+    }
+         else {
+    cout<<"false"<<endl;
+                }   }
+            
         }
         
-    }
+    
 
     return 0;
 }
