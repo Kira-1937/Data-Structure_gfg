6f8ace195b2734e18c6a48808eb021eb5eb4af7d
kira-1937 <vikramaryasaharan@gmail.com>
2021-06-17 12:40:07 +0530

    #define uses

---
+++ uses_of_define.cpp
@@ -0,0 +1,10 @@
+using namespace std;
+#include<iostream>
+#define PI 3.14
+int main(){
+    int r;
+    cout<<"value of radius is ";
+    cin>>r;
+    cout<<"area of circle is "<<(PI)*(r)*(r);
+    return 0;
+}
